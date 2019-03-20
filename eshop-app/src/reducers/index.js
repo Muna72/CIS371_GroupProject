6847ca131bf6917321840374b648d09e6397f718
KmanT <kturske@gmail.com>
2019-03-19 22:53:09 -0400

    Began the boiler plate for Redux within our application
    Since that we will be dealing with multiple pages within our app, I added some necessary files in order to get started with Redux

---
+++ eshop-app/src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+
+/**This is where all of the reducers are placed */
+export default combineReducers({
+
+});
---
+++ eshop-app/src/store.js
@@ -0,0 +1,1 @@
+/** This is where the state management occurs with Redux */
