8f10af40c7a0627af55b47775d63e8ffe0bb82fb
Trevor West <trevorawest97@gmail.com>
2019-03-22 13:49:42 -0400

    Router implementation

--- eshop-app/src/App.js
+++ eshop-app/src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+//import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
