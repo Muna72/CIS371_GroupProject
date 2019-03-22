b29adcaa6634fc13c453e1cb3604cc61de069575
Trevor West <trevorawest97@gmail.com>
2019-03-22 17:05:04 -0400

    Skeleton app layout

--- eshop-app/src/App.js
+++ eshop-app/src/App.js
@@ -1,29 +1,45 @@
-import React, { Component } from "react";
-//import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import logo from "./logo.svg";
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+function Index() {
+  return <h2>Home</h2>;
 }
 
-export default App;
+function Shop() {
+  return <h2>Shop</h2>;
+}
+
+function Account() {
+  return <h2>Account</h2>;
+}
+
+function AppRouter() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <div class="menuItems">
+              <li>
+                <Link to="/Shop/">Shop</Link>
+              </li>
+              <li>
+                <Link to="/Account/">Account</Link>
+              </li>
+            </div>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={Index} />
+        <Route path="/Shop/" component={Shop} />
+        <Route path="/Account/" component={Account} />
+      </div>
+    </Router>
+  );
+}
+
+export default AppRouter;
